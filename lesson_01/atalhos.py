9fb74dec7882492c763f0398bab9f3719ae4b2af
RuthdeSouza <ruthsouza7@gmail.com>
2025-04-25 20:07:51 -0300

    lesson_01/atalhos.py

---
+++ lesson_01/atalhos.py
@@ -0,0 +1,7 @@
+# ctrl + ; - inserir 'hastag', marcando o trecho como comentário
+# ctrl + alt + seta para baixo - duplicar 
+# ctrl + s - salvar alterações no vs code 
+
+nome = 1
+rua = 2
+print(nome, rua)
---
+++ lesson_01/explicacoesiniciais.py
@@ -0,0 +1,8 @@
+# Para inserir comentários, não interfere no código.
+# print: função para exibir informações na tela
+# Variavéis:
+# str: texto
+# int: números inteiros
+# float: números reais, inclusive os fracionários, ex.: 8.5, 0.9, 5.7
+# bool: são valores lógicos, verdadeiro ou falso
+# type: exibe o tipo de variável utilizada, ex.: type()
