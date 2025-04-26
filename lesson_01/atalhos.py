b31f60f0a127659010e8dd49e2c7001377e86315
RuthdeSouza <ruthsouza7@gmail.com>
2025-04-25 21:06:14 -0300

    Update atalhos.py

--- lesson_01/atalhos.py
+++ lesson_01/atalhos.py
@@ -1,7 +1,3 @@
 # ctrl + ; - inserir 'hastag', marcando o trecho como comentário
 # ctrl + alt + seta para baixo - duplicar 
 # ctrl + s - salvar alterações no vs code 
-
-nome = 1
-rua = 2
-print(nome, rua)
